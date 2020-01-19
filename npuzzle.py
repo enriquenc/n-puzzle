985647d8dc1a72f225e499ce23d5f0e97607577a
Maslianko Taras <tarasmaslianko@gmail.com>
2020-01-19 21:54:31 +0200

    added parser

--- npuzzle.py
+++ npuzzle.py
@@ -1,40 +1,40 @@
-#from puzzle_parser import *
+from puzzle_parser import *
 from algorithm import *
 from error import *
 import time
 
 def main():
-    #parser = Parser()
-    #parser.find_size()
-    puzzle = [[1, 2, 3],
-              [0, 8, 4],
-              [7, 6, 5]]
 
-    puzzle1 = [[5, 7, 1],
-               [6, 0, 2],
-               [4, 3, 8]]
+    # puzzle = [[1, 2, 3],
+    #           [0, 8, 4],
+    #           [7, 6, 5]]
+    #
+    # puzzle1 = [[5, 7, 1],
+    #            [6, 0, 2],
+    #            [4, 3, 8]]
+    #
+    # puzzle2 = [[1, 5, 3],
+    #            [2, 0, 4],
+    #            [6, 8, 7]]
+    #
+    # puzzle4 = [[1, 3, 6, 4],
+    #            [5, 2, 12, 8],
+    #            [15, 13, 7, 14],
+    #            [11, 10, 0, 9]]
 
-    puzzle2 = [[1, 5, 3],
-               [2, 0, 4],
-               [6, 8, 7]]
-
-    puzzle4 = [[1, 3, 6, 4],
-               [5, 2, 12, 8],
-               [15, 13, 7, 14],
-               [11, 10, 0, 9]]
-
-    puzzle = []
-    f = open("file", 'r')
-    size = int(f.readline())
-    for i in range(size):
-        line = f.readline().strip(' \n')
-        line = line.split(' ')
-        a = []
-        for j in line:
-            a.append(int(j))
-        puzzle.append(a)
-
-    a = Algorithm(puzzle, size)
+    # puzzle = []
+    # f = open("file", 'r')
+    # size = int(f.readline())
+    # for i in range(size):
+    #     line = f.readline().strip(' \n')
+    #     line = line.split(' ')
+    #     a = []
+    #     for j in line:
+    #         a.append(int(j))
+    #     puzzle.append(a)
+    parser = Parser()
+    parser.get_input()
+    a = Algorithm(parser.puzzle, parser.size)
     if a.is_already_solved():
         success(Success.ALREADY_SOLVED)
     #if a.is_solvable() is False:
---
+++ puzzle_parser.py
@@ -0,0 +1,97 @@
+import fileinput
+import re
+from error import Error, error
+
+#!TODO Возможность сохранения комментариев в файл по флагу.
+#!TODO Добавить парсер флагов
+
+
+class Parser:
+    def __init__(self):
+        self.size = 0
+        self.puzzle_elements = []
+        self.max_value = 0
+        self.puzzle = []
+
+
+    def find_size(self, line):
+        line = self.clear_line(line)
+        if line == "#":
+            return
+        elif re.match(r"^[0-9]*$", line) is not None:
+            self.size = int(line)
+        else:
+            error(Error.ERROR_SIZE)
+
+        if self.size < 3:
+            error(Error.ERROR_MIN_SIZE)
+
+        self.puzzle_elements = [[0] * (self.size * self.size)][0]
+        self.max_value = self.size * self.size - 1
+
+    @staticmethod
+    def clear_line(line):
+        line = line.rstrip(' \n')
+        if line == "":
+            error(Error.ERROR_EMPTY_LINE)
+        line = line.split('#')
+        if line[0] == "":
+            return "#"
+        return line[0].strip()
+
+    def find_puzzle(self, line):
+
+        line = self.clear_line(line)
+        if line == "#":
+            return False
+
+        if re.match(r"^[0-9 ]*$", line) is None:
+            error(Error.ERROR_ROW_ITEMS)
+
+        line = line.split(' ')
+
+        for l in line:
+            if l == '':
+                line.remove(l)
+
+        if len(line) != self.size:
+            error(Error.ERROR_ELEMENTS_AMOUNT, "Should be " + str(self.size) + " elements.")
+
+        self.check_row(line)
+        for i in range(len(line)):
+            line[i] = int(line[i])
+        self.puzzle.append(line)
+
+        if len(self.puzzle) == self.size:
+            return True
+
+    def check_row(self, line):
+        for elem in line:
+            elem = int(elem)
+            if elem > self.max_value:
+                error(Error.ERROR_BIG_ELEMENT, " Max element - " + str(self.max_value) + '.')
+            if self.puzzle_elements[elem] == 0:
+                self.puzzle_elements[elem] = 1
+            else:
+                error(Error.ERROR_ELEMENT_EXISTS, str(elem) + '.')
+
+    def get_input(self):
+        for line in fileinput.input():
+            if self.size == 0:
+                self.find_size(line)
+            elif self.find_puzzle(line) is True:
+                break
+        fileinput.close()
+
+
+
+if __name__ == "__main__":
+    p = Parser()
+    p.find_size()
+    print(p.get_puzzle())
+
+
+
+
+
+
