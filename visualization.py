2a5a05d68a18529a014509aa20bb73e16e1a93d7
Maslianko Taras <tarasmaslianko@gmail.com>
2020-02-08 00:37:45 +0200

    [Visualization] Add able to dinamicaly change the color of Board.

--- visualization.py
+++ visualization.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from tkinter import Canvas, BOTH
 import time
 
 class Point():
@@ -10,17 +10,25 @@ class Visualization(Canvas):
     def __init__(self, puzzle_solving, size, master=None, bg="white"):
         super().__init__(master, height=size * 100, width=size * 100, bg=bg)
         self.master = master
+        self.master.title("n-puzzle")
         self.puzzle_solving = puzzle_solving
         self.puzzle_solving.reverse()
         self.size = size
-
-        self.auto = True
+        self.colors = ["blue", "white", "red", "green", "cyan", "yellow", "magenta"]
+        self.current_color = 0
+        self.auto = False
         self.index = 0
         self.print_puzzle(puzzle_solving[self.index])
+        self.master.bind('<plus>', self.change_color)
         self.master.bind('<Left>', self.left_button_handler)
         self.master.bind('<Right>', self.right_button_handler)
         self.master.bind('<Button-1>', self.button_handler)
 
+    def change_color(self, button=None):
+        self.current_color += 1
+        if self.current_color >= len(self.colors):
+            self.current_color = 0
+        self.print_puzzle(self.puzzle_solving[self.index])
 
     def left_button_handler(self, button=None):
         self.auto = False
@@ -37,7 +45,9 @@ class Visualization(Canvas):
 
 
     def button_handler(self, button=None):
-        if button is not None:
+        if button is not None and self.auto is True:
+            return
+        elif button is not None:
             self.auto = True
         if self.index + 1 < len(self.puzzle_solving):
             self.index += 1
@@ -53,7 +63,7 @@ class Visualization(Canvas):
             if puzzle[i] == 0:
                 color = "white"
             else:
-                color = "red"
+                color = self.colors[self.current_color]
             top_left = Point(100 * (i % self.size), 100 * (i // self.size))
             botto_right = Point(100 + 100 * (i % self.size), 100 + 100 * (i // self.size))
             self.create_rectangle(top_left.x, top_left.y, botto_right.x, botto_right.y, fill=color)
